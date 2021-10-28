beee2361ebab35e98e91c3674d116ae9d57b4b39
Sathishkumar AJ <ajsatix@gmail.com>
2021-10-29 00:08:40 +0530

    Update and rename BeforeInstallHook.sh to before_install.sh

--- scripts/BeforeInstallHook.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-set -e
-sudo apt update -y
-sudo pm2 update
---
+++ scripts/before_install.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+set -e
+export PORT=3000
+export API_HOST=http://app-elb-967138747.us-east-1.elb.amazonaws.com/app
